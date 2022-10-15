5a62fbcd3d709c02d539224bbdddd2d4e2dcdbae
Konstantin Bozdugan <konstantin.bozdugan63@gmail.com>
2022-10-15 12:52:29 +0200

    DB support added

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,16 +1,23 @@
 from flask import Flask, render_template
+from webapp.model import db, News
 
-from .weather import weather_by_city
-from webapp.python_news import get_python_news
+
+from webapp.weather import weather_by_city
+
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
+    db.init_app(app)
+
     @app.route("/")
     def index():
         title = "Python News"
         weather = weather_by_city(app.config['WEATHER_DEFAULT_CITY'])
-        news_list = get_python_news()
+        news_list = News.query.order_by(News.published.desc()).all()
         return render_template("index.html", title=title, weather=weather, news_list=news_list)
 
     return app
+
+if __name__ == '__main__':
+    create_app()
